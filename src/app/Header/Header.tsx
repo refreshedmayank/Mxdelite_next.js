1132f44a2df8e30a44b38d498f8c2d84ca67d787
refreshedmayank <refreshedmayank@gmail.com>
2026-04-21 12:44:39 +0530

    styled Header

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -4,12 +4,15 @@ import { useState } from "react";
 function Header(){
 const [isopen, setisOpen] = useState(false);
 return(
-   <div className="Header-container header text-white">
+    <>
+   <div className="Header-container  flex justify-between items-center px-6 py-4 fixed top-0 left-0 w-full z-50 bg-black text-white">
     <button onClick={()=> setisOpen(!isopen)}>{isopen? "x" : "☰"}</button>
    <span>Mxdelite</span>
-   <div></div>
-   {isopen&& <MenuBar/>}
+   <div className="w-10"></div>
    </div>
+   {isopen&& <MenuBar/>}
+   </>
+
 );
 }
 export default Header;
--- src/app/Menu Bar/MenuBar.tsx
+++ src/app/Menu Bar/MenuBar.tsx
@@ -1,17 +1,11 @@
 import Link from "next/link";
 function MenuBar(){
 return(
-    <div className="Menu-container ">
+    <div className="Menu-container fixed top-14 bg-black text-white w-full h-full flex flex-col justify-center items-center gap-10 z-40">
     <Link href="/contact">Contact</Link>
     <Link href="/Services">Services</Link>
     <Link href="/contact">Problems</Link>
-    <Link href="/About">About</Link>
-
-
-
-        
-        
-    </div>
+    <Link href="/About">About</Link></div>
 
     );
 }
