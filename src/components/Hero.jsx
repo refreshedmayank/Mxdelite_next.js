43b2e6617f7e61ea3942409bab08a512c5b8155b
refreshedmayank <refreshedmayank@gmail.com>
2026-04-22 12:43:50 +0530

    styled hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,18 +3,17 @@ import SecondHero from "./secondhero";
 function Hero() {
   return (
     <>
-      <div className="hero">
-        <section className="max-w-2xl text-center space-y-6">
-          <h1 className="text-4xl md:text-5xl font-bold leading-tight">
+      <div className=" min-h-[calc(100vh-40px)] flex flex-col justify-center items-center p-6">
+          <h1 className="text-center text-3xl mb-8">
             We turn invisible businesses into brands that own the room.
           </h1>
 
-          <div className="flex justify-center gap-4 text-sm text-white/70">
+          <div className="flex flex-wrap gap-4 text-sm text-white/70">
             <span>Websites</span>
             <span>Brand Identity</span>
             <span>Social Media</span>
           </div>
-        </section>
+
       </div>
 
       <SecondHero />
